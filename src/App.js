3a275f7ac9bb8e2bb3c3248f815c219cde2db083
BEYZA YÜKSEL <69187640+beyzayuksell@users.noreply.github.com>
2022-02-17 12:37:47 +0300

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import List from "./List";
 function App() {
   const [people, setPeople] = useState(Data);
   return (
-    <main>
-      <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
-      </section>
-    </main>
+    <div className="app">
+      <main>
+        <section className="container">
+          <h3>{people.length} birthdays today</h3>
+          <List people={people} />
+          <button onClick={() => setPeople([])}>clear all</button>
+        </section>
+      </main>
+    </div>
   );
 }
 
